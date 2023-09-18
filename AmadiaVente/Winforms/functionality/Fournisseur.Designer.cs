53626bcb5461a8cffcbef8a3b6de25721106ef0d
wharton-git <whartonaldrick@gmail.com>
2023-09-18 20:57:39 +0300

    VTsou

--- AmadiaVente/Winforms/functionality/Fournisseur.Designer.cs
+++ AmadiaVente/Winforms/functionality/Fournisseur.Designer.cs
@@ -28,12 +28,283 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Fournisseur";
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges13 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges14 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges15 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges16 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges17 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges18 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges19 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges20 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges21 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges22 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges23 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges24 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            NomFournisseur = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            NomFournisseurs = new Guna.UI2.WinForms.Guna2TextBox();
+            ContactFourisseur = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            EmailFournisseur = new Guna.UI2.WinForms.Guna2TextBox();
+            guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            AdresseFournisseur = new Guna.UI2.WinForms.Guna2TextBox();
+            TableFournisseur = new Guna.UI2.WinForms.Guna2DataGridView();
+            btnAjoutFornisseur = new Guna.UI2.WinForms.Guna2GradientButton();
+            btnSupFournisseur = new Guna.UI2.WinForms.Guna2GradientButton();
+            ((System.ComponentModel.ISupportInitialize)TableFournisseur).BeginInit();
+            SuspendLayout();
+            // 
+            // NomFournisseur
+            // 
+            NomFournisseur.BackColor = Color.Transparent;
+            NomFournisseur.Location = new Point(64, 57);
+            NomFournisseur.Name = "NomFournisseur";
+            NomFournisseur.Size = new Size(94, 17);
+            NomFournisseur.TabIndex = 0;
+            NomFournisseur.Text = "Nom Fournisseur";
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Location = new Point(64, 95);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(45, 17);
+            guna2HtmlLabel1.TabIndex = 1;
+            guna2HtmlLabel1.Text = "Contact";
+            // 
+            // NomFournisseurs
+            // 
+            NomFournisseurs.CustomizableEdges = customizableEdges13;
+            NomFournisseurs.DefaultText = "";
+            NomFournisseurs.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            NomFournisseurs.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            NomFournisseurs.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            NomFournisseurs.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            NomFournisseurs.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            NomFournisseurs.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            NomFournisseurs.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            NomFournisseurs.Location = new Point(191, 47);
+            NomFournisseurs.Name = "NomFournisseurs";
+            NomFournisseurs.PasswordChar = '\0';
+            NomFournisseurs.PlaceholderText = "";
+            NomFournisseurs.SelectedText = "";
+            NomFournisseurs.ShadowDecoration.CustomizableEdges = customizableEdges14;
+            NomFournisseurs.Size = new Size(200, 36);
+            NomFournisseurs.TabIndex = 2;
+            // 
+            // ContactFourisseur
+            // 
+            ContactFourisseur.CustomizableEdges = customizableEdges15;
+            ContactFourisseur.DefaultText = "";
+            ContactFourisseur.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            ContactFourisseur.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            ContactFourisseur.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            ContactFourisseur.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            ContactFourisseur.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            ContactFourisseur.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            ContactFourisseur.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            ContactFourisseur.Location = new Point(191, 89);
+            ContactFourisseur.Name = "ContactFourisseur";
+            ContactFourisseur.PasswordChar = '\0';
+            ContactFourisseur.PlaceholderText = "";
+            ContactFourisseur.SelectedText = "";
+            ContactFourisseur.ShadowDecoration.CustomizableEdges = customizableEdges16;
+            ContactFourisseur.Size = new Size(200, 36);
+            ContactFourisseur.TabIndex = 2;
+            // 
+            // guna2HtmlLabel2
+            // 
+            guna2HtmlLabel2.BackColor = Color.Transparent;
+            guna2HtmlLabel2.Location = new Point(64, 133);
+            guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            guna2HtmlLabel2.Size = new Size(32, 17);
+            guna2HtmlLabel2.TabIndex = 1;
+            guna2HtmlLabel2.Text = "Email";
+            // 
+            // EmailFournisseur
+            // 
+            EmailFournisseur.CustomizableEdges = customizableEdges17;
+            EmailFournisseur.DefaultText = "";
+            EmailFournisseur.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            EmailFournisseur.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            EmailFournisseur.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            EmailFournisseur.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            EmailFournisseur.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            EmailFournisseur.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            EmailFournisseur.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            EmailFournisseur.Location = new Point(191, 131);
+            EmailFournisseur.Name = "EmailFournisseur";
+            EmailFournisseur.PasswordChar = '\0';
+            EmailFournisseur.PlaceholderText = "";
+            EmailFournisseur.SelectedText = "";
+            EmailFournisseur.ShadowDecoration.CustomizableEdges = customizableEdges18;
+            EmailFournisseur.Size = new Size(200, 36);
+            EmailFournisseur.TabIndex = 2;
+            // 
+            // guna2HtmlLabel3
+            // 
+            guna2HtmlLabel3.BackColor = Color.Transparent;
+            guna2HtmlLabel3.Location = new Point(64, 179);
+            guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            guna2HtmlLabel3.Size = new Size(44, 17);
+            guna2HtmlLabel3.TabIndex = 1;
+            guna2HtmlLabel3.Text = "Adresse";
+            // 
+            // AdresseFournisseur
+            // 
+            AdresseFournisseur.CustomizableEdges = customizableEdges19;
+            AdresseFournisseur.DefaultText = "";
+            AdresseFournisseur.DisabledState.BorderColor = Color.FromArgb(208, 208, 208);
+            AdresseFournisseur.DisabledState.FillColor = Color.FromArgb(226, 226, 226);
+            AdresseFournisseur.DisabledState.ForeColor = Color.FromArgb(138, 138, 138);
+            AdresseFournisseur.DisabledState.PlaceholderForeColor = Color.FromArgb(138, 138, 138);
+            AdresseFournisseur.FocusedState.BorderColor = Color.FromArgb(94, 148, 255);
+            AdresseFournisseur.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            AdresseFournisseur.HoverState.BorderColor = Color.FromArgb(94, 148, 255);
+            AdresseFournisseur.Location = new Point(191, 173);
+            AdresseFournisseur.Name = "AdresseFournisseur";
+            AdresseFournisseur.PasswordChar = '\0';
+            AdresseFournisseur.PlaceholderText = "";
+            AdresseFournisseur.SelectedText = "";
+            AdresseFournisseur.ShadowDecoration.CustomizableEdges = customizableEdges20;
+            AdresseFournisseur.Size = new Size(200, 36);
+            AdresseFournisseur.TabIndex = 2;
+            // 
+            // TableFournisseur
+            // 
+            TableFournisseur.AllowUserToAddRows = false;
+            TableFournisseur.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle4.BackColor = Color.White;
+            TableFournisseur.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
+            TableFournisseur.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridViewCellStyle5.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = Color.FromArgb(100, 88, 255);
+            dataGridViewCellStyle5.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle5.ForeColor = Color.White;
+            dataGridViewCellStyle5.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = DataGridViewTriState.True;
+            TableFournisseur.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            TableFournisseur.ColumnHeadersHeight = 30;
+            TableFournisseur.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = Color.White;
+            dataGridViewCellStyle6.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            dataGridViewCellStyle6.ForeColor = Color.FromArgb(71, 69, 94);
+            dataGridViewCellStyle6.SelectionBackColor = Color.FromArgb(231, 229, 255);
+            dataGridViewCellStyle6.SelectionForeColor = Color.FromArgb(71, 69, 94);
+            dataGridViewCellStyle6.WrapMode = DataGridViewTriState.False;
+            TableFournisseur.DefaultCellStyle = dataGridViewCellStyle6;
+            TableFournisseur.GridColor = Color.FromArgb(231, 229, 255);
+            TableFournisseur.Location = new Point(1, 306);
+            TableFournisseur.Name = "TableFournisseur";
+            TableFournisseur.ReadOnly = true;
+            TableFournisseur.RowHeadersVisible = false;
+            TableFournisseur.RowTemplate.Height = 25;
+            TableFournisseur.Size = new Size(797, 132);
+            TableFournisseur.TabIndex = 7;
+            TableFournisseur.ThemeStyle.AlternatingRowsStyle.BackColor = Color.White;
+            TableFournisseur.ThemeStyle.AlternatingRowsStyle.Font = null;
+            TableFournisseur.ThemeStyle.AlternatingRowsStyle.ForeColor = Color.Empty;
+            TableFournisseur.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = Color.Empty;
+            TableFournisseur.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = Color.Empty;
+            TableFournisseur.ThemeStyle.BackColor = Color.White;
+            TableFournisseur.ThemeStyle.GridColor = Color.FromArgb(231, 229, 255);
+            TableFournisseur.ThemeStyle.HeaderStyle.BackColor = Color.FromArgb(100, 88, 255);
+            TableFournisseur.ThemeStyle.HeaderStyle.BorderStyle = DataGridViewHeaderBorderStyle.None;
+            TableFournisseur.ThemeStyle.HeaderStyle.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            TableFournisseur.ThemeStyle.HeaderStyle.ForeColor = Color.White;
+            TableFournisseur.ThemeStyle.HeaderStyle.HeaightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            TableFournisseur.ThemeStyle.HeaderStyle.Height = 30;
+            TableFournisseur.ThemeStyle.ReadOnly = true;
+            TableFournisseur.ThemeStyle.RowsStyle.BackColor = Color.White;
+            TableFournisseur.ThemeStyle.RowsStyle.BorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            TableFournisseur.ThemeStyle.RowsStyle.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            TableFournisseur.ThemeStyle.RowsStyle.ForeColor = Color.FromArgb(71, 69, 94);
+            TableFournisseur.ThemeStyle.RowsStyle.Height = 25;
+            TableFournisseur.ThemeStyle.RowsStyle.SelectionBackColor = Color.FromArgb(231, 229, 255);
+            TableFournisseur.ThemeStyle.RowsStyle.SelectionForeColor = Color.FromArgb(71, 69, 94);
+            // 
+            // btnAjoutFornisseur
+            // 
+            btnAjoutFornisseur.Anchor = AnchorStyles.Top;
+            btnAjoutFornisseur.BorderRadius = 15;
+            btnAjoutFornisseur.CustomizableEdges = customizableEdges21;
+            btnAjoutFornisseur.DisabledState.BorderColor = Color.DarkGray;
+            btnAjoutFornisseur.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnAjoutFornisseur.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnAjoutFornisseur.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnAjoutFornisseur.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnAjoutFornisseur.FillColor = Color.MediumSeaGreen;
+            btnAjoutFornisseur.FillColor2 = Color.MediumSeaGreen;
+            btnAjoutFornisseur.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAjoutFornisseur.ForeColor = Color.White;
+            btnAjoutFornisseur.Location = new Point(191, 239);
+            btnAjoutFornisseur.Name = "btnAjoutFornisseur";
+            btnAjoutFornisseur.ShadowDecoration.CustomizableEdges = customizableEdges22;
+            btnAjoutFornisseur.Size = new Size(140, 45);
+            btnAjoutFornisseur.TabIndex = 10;
+            btnAjoutFornisseur.Text = "Ajouter";
+            // 
+            // btnSupFournisseur
+            // 
+            btnSupFournisseur.Anchor = AnchorStyles.Top;
+            btnSupFournisseur.BorderRadius = 15;
+            btnSupFournisseur.CustomizableEdges = customizableEdges23;
+            btnSupFournisseur.DisabledState.BorderColor = Color.DarkGray;
+            btnSupFournisseur.DisabledState.CustomBorderColor = Color.DarkGray;
+            btnSupFournisseur.DisabledState.FillColor = Color.FromArgb(169, 169, 169);
+            btnSupFournisseur.DisabledState.FillColor2 = Color.FromArgb(169, 169, 169);
+            btnSupFournisseur.DisabledState.ForeColor = Color.FromArgb(141, 141, 141);
+            btnSupFournisseur.FillColor = Color.FromArgb(255, 128, 128);
+            btnSupFournisseur.FillColor2 = Color.FromArgb(255, 128, 128);
+            btnSupFournisseur.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSupFournisseur.ForeColor = Color.White;
+            btnSupFournisseur.Location = new Point(488, 239);
+            btnSupFournisseur.Name = "btnSupFournisseur";
+            btnSupFournisseur.ShadowDecoration.CustomizableEdges = customizableEdges24;
+            btnSupFournisseur.Size = new Size(140, 45);
+            btnSupFournisseur.TabIndex = 32;
+            btnSupFournisseur.Text = "Supprimer";
+            // 
+            // Fournisseur
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnSupFournisseur);
+            Controls.Add(btnAjoutFornisseur);
+            Controls.Add(TableFournisseur);
+            Controls.Add(AdresseFournisseur);
+            Controls.Add(EmailFournisseur);
+            Controls.Add(ContactFourisseur);
+            Controls.Add(NomFournisseurs);
+            Controls.Add(guna2HtmlLabel3);
+            Controls.Add(guna2HtmlLabel2);
+            Controls.Add(guna2HtmlLabel1);
+            Controls.Add(NomFournisseur);
+            Name = "Fournisseur";
+            Text = "Fournisseur";
+            ((System.ComponentModel.ISupportInitialize)TableFournisseur).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2HtmlLabel NomFournisseur;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2TextBox NomFournisseurs;
+        private Guna.UI2.WinForms.Guna2TextBox ContactFourisseur;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2TextBox EmailFournisseur;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+        private Guna.UI2.WinForms.Guna2TextBox AdresseFournisseur;
+        private Guna.UI2.WinForms.Guna2DataGridView TableFournisseur;
+        private Guna.UI2.WinForms.Guna2GradientButton btnAjoutFornisseur;
+        private Guna.UI2.WinForms.Guna2GradientButton btnSupFournisseur;
     }
 }
