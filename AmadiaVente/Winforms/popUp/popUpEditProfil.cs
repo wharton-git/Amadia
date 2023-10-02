9d72a7392a099feeff71bde445d59ef490a421f0
Wharton Aldrick <whartonaldrick@gmail.com>
2023-10-03 02:24:28 +0300

    deplacement pop up

--- AmadiaVente/Winforms/popUp/popUpEditProfil.cs
+++ AmadiaVente/Winforms/popUp/popUpEditProfil.cs
@@ -13,12 +13,47 @@ namespace AmadiaVente.Winforms.popUp
 {
     public partial class popUpEditProfil : Form
     {
+
+        private bool isDragging = false;
+        private Point lastCursorPos;
+        private Point lastFormPos;
+
         public popUpEditProfil()
         {
             InitializeComponent();
+
+            this.MouseDown += (sender, e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    isDragging = true;
+                    lastCursorPos = Cursor.Position;
+                    lastFormPos = this.Location;
+                }
+            };
+
+            // Gérez l'événement MouseMove pour déplacer le formulaire
+            this.MouseMove += (sender, e) =>
+            {
+                if (isDragging)
+                {
+                    Point delta = new Point(Cursor.Position.X - lastCursorPos.X, Cursor.Position.Y - lastCursorPos.Y);
+                    this.Location = new Point(lastFormPos.X + delta.X, lastFormPos.Y + delta.Y);
+                }
+            };
+
+            // Gérez l'événement MouseUp pour arrêter de déplacer le formulaire
+            this.MouseUp += (sender, e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    isDragging = false;
+                }
+            };
         }
 
-        private void popUpEditProfil_Load(object sender, EventArgs e)
+
+    private void popUpEditProfil_Load(object sender, EventArgs e)
         {
             panelEditMdp.Visible = false;
         }
