399fdaa167f7b8d6cb1cc75845c73ee74fb32eb1
Wharton Aldrick <whartonaldrick@gmail.com>
2023-10-10 04:52:37 +0300

    correction VTsou

--- AmadiaVente/Winforms/functionality/Fournisseur.cs
+++ AmadiaVente/Winforms/functionality/Fournisseur.cs
@@ -21,23 +21,31 @@ namespace AmadiaVente.Winforms.functionality
 
         private void btnAjoutFornisseur_Click(object sender, EventArgs e)
         {
-            string connectionString = "Data Source=mydatabase.db;Version=3;";
+            string connectionString = "Data Source=" + System.IO.Path.Combine(Application.StartupPath, "../../../database.db");
             using (SQLiteConnection connection = new SQLiteConnection(connectionString))
             {
+
+                string nomF = NomFournisseurs.Text;
+                string contact = ContactFourisseur.Text;
+                int contactF = Convert.ToInt32(contact);
+                string emailF = EmailFournisseur.Text;
+                string adresseF = AdresseFournisseur.Text;
+
                 connection.Open();
 
                 // 3. Créez une commande SQL d'insertion
-                string sql = "INSERT INTO fournisseur (nomFournisseur, contact, email, adresse) VALUES (@NomFournisseur, @ContactFournisseur, @EmailFounisseur, @AdresseFournisseur)";
+                string sql = "INSERT INTO fournisseur (nomFournisseur, contact, email, adresse) VALUES (@NomFournisseur, @ContactFournisseur, @EmailFournisseur, @AdresseFournisseur)";
                 using (SQLiteCommand co = new SQLiteCommand(sql, connection))
                 {
                     // 4. Ajoutez des paramètres pour les valeurs que vous souhaitez insérer
-                    co.Parameters.AddWithValue("@NomFournisseur", NomFournisseur.Text); // Utilisez le contrôle NomFournisseur.Text ici
-                    co.Parameters.AddWithValue("@ContactFournisseur", ContactFourisseur.Text); // Utilisez le contrôle ContactFourisseur.Text ici
-                    co.Parameters.AddWithValue("@EmailFournisseur", EmailFournisseur.Text); // Utilisez le contrôle EmailFournisseur.Text ici
-                    co.Parameters.AddWithValue("@AdresseFournisseur", AdresseFournisseur.Text); // Utilisez le contrôle AdresseFournisseur.Text ici
+                    co.Parameters.AddWithValue("@NomFournisseur", nomF); // Utilisez le contrôle NomFournisseur.Text ici
+                    co.Parameters.AddWithValue("@ContactFournisseur", contactF); // Utilisez le contrôle ContactFourisseur.Text ici
+                    co.Parameters.AddWithValue("@EmailFournisseur", emailF); // Utilisez le contrôle EmailFournisseur.Text ici
+                    co.Parameters.AddWithValue("@AdresseFournisseur", adresseF); // Utilisez le contrôle AdresseFournisseur.Text ici
 
                     // 5. Exécutez la commande SQL
                     co.ExecuteNonQuery();
+                    MessageBox.Show("Succes");
                 }
             }
         }
